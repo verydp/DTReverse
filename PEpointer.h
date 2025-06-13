4a0cfd6dc30fbd0a63c67e2545e848f91ff82f8b
verydp <845192030@qq.com>
2025-06-13 20:24:47 +0800

    创建了解析PE文件结构的指针函数库

--- PEpointer.h
+++ PEpointer.h
@@ -1,40 +1,6 @@
 #ifndef PE_POINTER_H
 #define PE_POINTER_H
 
-
-
-/*
-//PEpointerº¯Êý
-
-int* ptre_lfanew(char* fbuffer) {}
-
-char* ptrPE(char* fbuffer) {}
-
-char* ptrOptionPE(char* fbuffer) {}
-
-int* ptrSizeOfImage(char* fbuffer) {}
-
-int* ptrSizeOfHeaders(char* fbuffer) {}
-
-short* ptrSizeOfOptionalHeader(char* fbuffer) {}
-
-char* ptrSection(char* fbuffer) {}
-
-int SectionOffset(char* fbuffer) {}
-
-int* ptrVirtualAddress(char* ptrSection) {}
-
-int* ptrSizeOfRawData(char* ptrSection) {}
-
-int* ptrPointerToRawData(char* ptrSection) {}
-
-int* ptrCharacteristics(char* ptrSection) {}
-
-short* ptrNumberOfSection(char* fbuffer) {}
-
-int* ptrImageBase(char* fbuffer) {}
-*/
-
 //PEpointerº¯Êý
 
 int* ptre_lfanew(char* fbuffer) {
